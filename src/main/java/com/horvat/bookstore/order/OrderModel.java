665cda5b35411ad63476662171e11952b45a2547
Horvat Horatiu <horvat.horatiu97@gmail.com>
2024-02-08 17:12:39 +0200

    order model update

--- src/main/java/com/horvat/bookstore/order/OrderModel.java
+++ src/main/java/com/horvat/bookstore/order/OrderModel.java
@@ -1,5 +1,11 @@
 package com.horvat.bookstore.order;
 
-public class OrderModel {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+public class OrderModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 }
