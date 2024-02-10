c923198b9c661234c22db80c05c33543bb386a93
Horvat Horatiu <horvat.horatiu97@gmail.com>
2024-02-10 08:30:46 +0200

    added getters and setters for order dto

--- src/main/java/com/horvat/bookstore/order/dtos/requests/ReqOrderDto.java
+++ src/main/java/com/horvat/bookstore/order/dtos/requests/ReqOrderDto.java
@@ -2,6 +2,10 @@ package com.horvat.bookstore.order.dtos.requests;
 
 import java.util.Map;
 
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class ReqOrderDto {
     private Integer id;
     private String address;
