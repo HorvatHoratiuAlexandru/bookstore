35380c50ab9f1bae9d0afe2c265423db3313c9f7
Horvat Horatiu <horvat.horatiu97@gmail.com>
2024-02-24 19:04:31 +0200

    jwt config properties

---
+++ src/main/java/com/horvat/bookstore/configs/JwtConfigProperties.java
@@ -0,0 +1,8 @@
+package com.horvat.bookstore.configs;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("myjwt")
+public record JwtConfigProperties(String jwtSecret) {
+
+}
